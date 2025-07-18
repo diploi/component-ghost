b05336dbdaf2dd2a8421ccb39307fc50f7747b86
Tuomas <me@tuomas.id>
2025-07-18 13:54:13 +0300

    Try fixing image storage

--- .diploi/diploi-storage-adapter.js
+++ .diploi/diploi-storage-adapter.js
@@ -1,16 +1,24 @@
-// # Local File System Storage module
-// The (default) module for storing media, using the local file system
+// # Local File System Image Storage module
+// The (default) module for storing images, using the local file system
 const config = require('../../../shared/config');
+const urlUtils = require('../../../shared/url-utils');
 const LocalStorageBase = require('./LocalStorageBase');
 
-class DiploiStorage extends LocalStorageBase {
+const messages = {
+  notFound: 'Image not found',
+  notFoundWithRef: 'Image not found: {file}',
+  cannotRead: 'Could not read image: {file}',
+};
+
+class DiploiImagesStorage extends LocalStorageBase {
   constructor() {
     super({
-      storagePath: '/data',
+      storagePath: '/data/images',
+      staticFileURLPrefix: urlUtils.STATIC_IMAGE_URL_PREFIX,
       siteUrl: config.getSiteUrl(),
-      staticFileURLPrefix: '',
+      errorMessages: messages,
     });
   }
 }
 
-module.exports = DiploiStorage;
+module.exports = DiploiImagesStorage;
