1ab342425786ccfd3dba6a79dc6328496290f93b
Tuomas <me@tuomas.id>
2025-07-18 12:19:35 +0300

    Try out an inherited storage adapter

--- .diploi/diploi-storage-adapter.js
+++ .diploi/diploi-storage-adapter.js
@@ -1,34 +1,15 @@
-const path = require('path');
-// const fs = require('fs');
-const BaseAdapter = require('ghost-storage-base');
-const express = require('express');
+// # Local File System Storage module
+// The (default) module for storing media, using the local file system
+const config = require('../../../shared/config');
+const LocalStorageBase = require('./LocalStorageBase');
 
-class DiploiStorage extends BaseAdapter {
+class DiploiStorage extends LocalStorageBase {
   constructor() {
-    super();
-    this.storagePath = '/data';
-  }
-
-  async save(image) {
-    const targetPath = path.join(this.storagePath, path.basename(image.path));
-    // await fs.copy(image.path, targetPath);
-    return `/content/images/${path.basename(image.path)}`; // URL returned
-  }
-
-  serve() {
-    return express.static(this.storagePath);
-  }
-
-  async delete(fileName) {
-    const targetPath = path.join(this.storagePath, path.basename(fileName));
-    // await fs.remove(targetPath);
-    return;
-  }
-
-  async exists(fileName) {
-    const targetPath = path.join(this.storagePath, path.basename(fileName));
-    // return fs.pathExists(targetPath);
-    return true;
+    super({
+      storagePath: '/data',
+      siteUrl: config.getSiteUrl(),
+      staticFileURLPrefix: '',
+    });
   }
 }
 
