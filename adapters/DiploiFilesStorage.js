c30206a1efe34b58f88869c30d164018e3eaf08c
giaongo <83873333+giaongo@users.noreply.github.com>
2026-05-07 14:48:27 +0300

    Fix getStaticUrlPrefix not a function

--- adapters/DiploiFilesStorage.js
+++ adapters/DiploiFilesStorage.js
@@ -2,6 +2,7 @@
 // The (default) module for storing media, modified to use the Diploi volume mount /data
 // This file will be moved to the /core/server/adapters/storage folder by the Dockerfile.
 const config = require('../../../shared/config');
+const urlUtils = require('../../../shared/url-utils');
 const LocalStorageBase = require('./LocalStorageBase');
 
 const messages = {
@@ -15,7 +16,7 @@ class DiploiFilesStorage extends LocalStorageBase {
     super({
       storagePath: '/data/files',
       siteUrl: config.getSiteUrl(),
-      staticFileURLPrefix: config.getStaticUrlPrefix('files'),
+      staticFileURLPrefix: urlUtils.STATIC_FILES_URL_PREFIX,
       errorMessages: messages,
     });
   }
--- adapters/DiploiMediaStorage.js
+++ adapters/DiploiMediaStorage.js
@@ -2,6 +2,7 @@
 // The (default) module for storing media, modified to use the Diploi volume mount /data
 // This file will be moved to the /core/server/adapters/storage folder by the Dockerfile.
 const config = require('../../../shared/config');
+const urlUtils = require('../../../shared/url-utils');
 const LocalStorageBase = require('./LocalStorageBase');
 
 const messages = {
@@ -14,7 +15,7 @@ class DiploiMediaStorage extends LocalStorageBase {
   constructor() {
     super({
       storagePath: '/data/media',
-      staticFileURLPrefix: config.getStaticUrlPrefix('media'),
+      staticFileURLPrefix: urlUtils.STATIC_MEDIA_URL_PREFIX,
       siteUrl: config.getSiteUrl(),
       errorMessages: messages,
     });
